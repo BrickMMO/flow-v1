9237fc13c4cfd980ddb0396d34d42659ff72eafe
Adam Thomas <thomasadam83@hotmail.com>
2023-08-02 23:08:04 -0400

    Added MySQL export

---
+++ flow.sql
@@ -0,0 +1,314 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Aug 03, 2023 at 03:07 AM
+-- Server version: 5.7.39
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `brickmmo_flow`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `first` varchar(255) DEFAULT NULL,
+  `last` varchar(255) DEFAULT NULL,
+  `github` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `status` enum('active','inactive') NOT NULL DEFAULT 'active',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `upated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `first`, `last`, `github`, `email`, `password`, `status`, `created_at`, `upated_at`) VALUES
+(1, 'Adam', 'Smith', 'codeadamca', 'adam.smith@email.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'active', '2023-07-30 20:07:45', '2023-08-03 03:04:17'),
+(2, 'James', 'Doe', '', 'james.doe@email.ca', '5f4dcc3b5aa765d61d8327deb882cf99', 'active', '2023-07-31 20:16:12', '2023-08-03 03:04:40');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `classes`
+--
+
+CREATE TABLE `classes` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  `semester` enum('fall','winter','summer') DEFAULT NULL,
+  `year` year(4) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `classes`
+--
+
+INSERT INTO `classes` (`id`, `name`, `code`, `semester`, `year`, `created_at`, `updated_at`) VALUES
+(1, 'Workshops in Web Development 1', 'HTTP5114', 'fall', 2023, '2023-07-30 19:15:28', '2023-07-30 19:16:10'),
+(2, 'Workshops in Web Development 2', 'HTTP5214', 'winter', 2023, '2023-07-30 19:15:28', '2023-07-31 03:30:28');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class_student`
+--
+
+CREATE TABLE `class_student` (
+  `id` int(11) NOT NULL,
+  `class_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `class_student`
+--
+
+INSERT INTO `class_student` (`id`, `class_id`, `student_id`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(4, 1, 3),
+(7, 1, 4),
+(9, 1, 6),
+(3, 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class_task`
+--
+
+CREATE TABLE `class_task` (
+  `id` int(11) NOT NULL,
+  `class_id` int(11) NOT NULL,
+  `task_id` int(11) NOT NULL,
+  `due_at` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `class_task`
+--
+
+INSERT INTO `class_task` (`id`, `class_id`, `task_id`, `due_at`) VALUES
+(25, 1, 1, '2023-09-15'),
+(26, 1, 3, '2023-09-29'),
+(27, 2, 2, '2023-08-17'),
+(28, 2, 1, '2023-08-30');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `description` text,
+  `task_id` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `first` varchar(255) DEFAULT NULL,
+  `last` varchar(255) DEFAULT NULL,
+  `github` varchar(255) DEFAULT NULL,
+  `linkedin` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `status` enum('active','inactive') NOT NULL DEFAULT 'active',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `first`, `last`, `github`, `linkedin`, `email`, `password`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'Adam', 'Smith', 'codeadamca', 'adambenjaminthomas', 'adam.smith@email.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'active', '2023-07-30 19:58:20', '2023-08-03 03:06:54'),
+(2, 'James', 'Smith', NULL, NULL, 'james.smith@email.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'active', '2023-07-31 03:29:21', '2023-08-03 03:05:37'),
+(3, 'Jean', 'LastGray', NULL, NULL, 'jean@address.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'active', '2023-07-31 21:22:44', '2023-08-03 03:07:06'),
+(4, 'James', 'Smith', NULL, NULL, 'james@email.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'active', '2023-07-31 21:32:12', '2023-08-03 03:05:46'),
+(6, 'Jake', 'Smith', NULL, NULL, 'jake.smith@email.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'active', '2023-08-01 21:18:24', '2023-08-01 21:18:24');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_task`
+--
+
+CREATE TABLE `student_task` (
+  `id` int(11) NOT NULL,
+  `student_id` int(11) DEFAULT NULL,
+  `task_id` int(11) DEFAULT NULL,
+  `class_id` int(11) DEFAULT NULL,
+  `completed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `description` text,
+  `url` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tasks`
+--
+
+INSERT INTO `tasks` (`id`, `name`, `description`, `url`, `created_at`, `updated_at`) VALUES
+(1, 'GitHub Profile', 'Mauris magna arcu, tincidunt vitae libero vel, scelerisque laoreet quam. Curabitur convallis, arcu non mollis pulvinar.', 'https://github.com/BrickMMO/flow-tasks', '2023-07-31 03:56:29', '2023-07-31 04:08:44'),
+(2, 'Accounts', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porttitor iaculis mi sed posuere. ', 'https://github.com/BrickMMO/flow-tasks', '2023-07-31 04:03:23', '2023-08-01 23:13:14'),
+(3, 'Repos for Everything', 'Aenean porta, magna euismod eleifend interdum, nisi dui egestas felis, in tristique justo nisi ut sem. Etiam maximus massa a feugiat tincidunt.', 'https://github.com/BrickMMO/flow-tasks', '2023-08-01 23:13:58', '2023-08-01 23:13:58');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `classes`
+--
+ALTER TABLE `classes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `class_student`
+--
+ALTER TABLE `class_student`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `class_id` (`class_id`,`student_id`);
+
+--
+-- Indexes for table `class_task`
+--
+ALTER TABLE `class_task`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `class_id` (`class_id`,`task_id`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `student_task`
+--
+ALTER TABLE `student_task`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `classes`
+--
+ALTER TABLE `classes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `class_student`
+--
+ALTER TABLE `class_student`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `class_task`
+--
+ALTER TABLE `class_task`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `student_task`
+--
+ALTER TABLE `student_task`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
