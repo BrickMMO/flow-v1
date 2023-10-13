4baec22daf8e6da68d96c954f7f7c3df2182db45
Adam <thomasadam83@hotmail.com>
2023-10-13 16:47:33 -0400

    Added task join table

--- imports/flow-sandbox.sql
+++ imports/flow-sandbox.sql
@@ -489,3 +489,54 @@ CREATE TABLE `entries` (
 
 -- --------------------------------------------------------
 
+-- phpMyAdmin SQL Dump
+-- version 5.1.1deb5ubuntu1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Oct 13, 2023 at 08:47 PM
+-- Server version: 8.0.34-0ubuntu0.22.04.1
+-- PHP Version: 8.1.2-1ubuntu2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Database: `brickmmo_flow`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `skill_student`
+--
+
+CREATE TABLE `skill_student` (
+  `id` int NOT NULL,
+  `skill_id` int NOT NULL,
+  `student_id` int NOT NULL,
+  `rating` int NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `skill_student`
+--
+ALTER TABLE `skill_student`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `skill_student`
+--
+ALTER TABLE `skill_student`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+COMMIT;
