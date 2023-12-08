71164fef4b2d379a106f9568c2d915cf1ad26aac
Lovepreet <gillpreetsingh35@gmail.com>
2023-12-08 13:07:49 -0500

    pagination is done

--- questions.php
+++ questions.php
@@ -14,18 +14,44 @@ include('includes/header.php');
 
 <?php
 
-$query = "SELECT 
+$resultsPerPage =10;
+
+$totalrows = "SELECT * From questions";
+$rows= mysqli_query($connect,$totalrows);
+$numberOfRows = mysqli_num_rows($rows);
+
+//Determining total number of pages
+$numberOfPage= ceil($numberOfRows/$resultsPerPage);
+
+// detemining which page number visitor is currentlt on
+if(!isset($_GET['page']))
+{
+    $page = 1;
+}
+else
+{
+    $page = $_GET['page'];
+}
+
+// determine the sql LIMIT starting number for the results on the displaying page
+
+$start =($page-1)* $resultsPerPage;
+
+
+
+$query = 'SELECT 
             q.id as id ,
             q.question,
             q.student_id,
             q.created_at ,
-            CONCAT(s.first, ' ', s.last) AS name ,
+            CONCAT(s.first," ", s.last) AS name ,
             COUNT(a.question_id) AS answers 
             FROM questions q 
             JOIN students s ON q.student_id = s.id 
             LEFT JOIN answers AS a ON a.question_id = q.id 
             GROUP BY q.id 
-            ORDER BY created_at DESC";
+            ORDER BY created_at DESC
+            LIMIT '.$start.','.$resultsPerPage.'';
 $result = mysqli_query($connect, $query);
 
 
@@ -54,8 +80,35 @@ $result = mysqli_query($connect, $query);
             <a href="question_details.php?id=<?=$task['id']?>">Show Answers</a>
         </div>
     </div>
-<?php endwhile; ?>
-
+    
+<?php endwhile; 
+$BufferLinks = 2 ?>
+<div id="pageLinks">
+        <ul>
+            <li  <?php if ($page==1) {
+                    echo "class='current'";
+                }?>> <a href="questions.php?page=1">First</a></li>
+            <li>
+                <?php if($page > $BufferLinks+1): ?>
+                ...
+                <?php endif;?>
+            </li>
+            <?php for ($i=max(1,$page-$BufferLinks); $i <=min($page+$BufferLinks,$numberOfPage) ; $i++):?>
+                <li  <?php if ($i==$page) {
+                    echo "class='current'";
+                }?>>
+                <a href="questions.php?page=<?=$i?>"><?=$i?></a></li>
+                <?php endfor;?>
+            <li>
+            <?php if ($page < $numberOfPage - $BufferLinks): ?>
+                ... 
+                <?php endif;?>
+            </li>
+            <li <?php if ($page==$numberOfPage) {
+                    echo "class='current'";
+                }?>> <a href='questions.php?page=<?=$numberOfPage?>'>Last</a></li>
+        </ul>
+    </div>
 
 <?php
 
